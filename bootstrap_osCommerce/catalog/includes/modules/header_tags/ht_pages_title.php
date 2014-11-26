86608819ddbdebe1c850376d41b6745c432d6ed9
clustersolutions <clustersolutions@users.noreply.github.com>
2014-11-25 19:52:01 -0800

    Added user defined separator to title tags.

--- bootstrap_osCommerce/catalog/includes/modules/header_tags/ht_pages_title.php
+++ bootstrap_osCommerce/catalog/includes/modules/header_tags/ht_pages_title.php
@@ -32,7 +32,7 @@
       global $oscTemplate;
 
       if ( (defined('META_SEO_TITLE')) && (strlen(META_SEO_TITLE) > 0) ) {
-        $oscTemplate->setTitle(tep_output_string(META_SEO_TITLE)  . ', ' . $oscTemplate->getTitle());
+        $oscTemplate->setTitle(tep_output_string(META_SEO_TITLE)  . MODULE_HEADER_TAGS_PAGES_TITLE_SEPARATOR . $oscTemplate->getTitle());
       }
 
     }
@@ -48,6 +48,7 @@
     function install() {
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Product Title Module', 'MODULE_HEADER_TAGS_PAGES_TITLE_STATUS', 'True', 'Do you want to allow product titles to be added to the page title?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_HEADER_TAGS_PAGES_TITLE_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
+      tep_db_query( "insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Separator', 'MODULE_HEADER_TAGS_PAGES_TITLE_SEPARATOR', '-', 'The separator to put between this element and the following element.', '6', '2', now())" );
     }
 
     function remove() {
@@ -55,7 +56,7 @@
     }
 
     function keys() {
-      return array('MODULE_HEADER_TAGS_PAGES_TITLE_STATUS', 'MODULE_HEADER_TAGS_PAGES_TITLE_SORT_ORDER');
+      return array('MODULE_HEADER_TAGS_PAGES_TITLE_STATUS', 'MODULE_HEADER_TAGS_PAGES_TITLE_SORT_ORDER', 'MODULE_HEADER_TAGS_PAGES_TITLE_SEPARATOR');
     }
   }
 ?>
